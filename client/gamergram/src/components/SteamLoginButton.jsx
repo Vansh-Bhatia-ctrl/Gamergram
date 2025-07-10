b217c27ad53f75ac9e1807d6470e13d7672f30a8
Vansh-Bhatia-ctrl <vanshb066@gmail.com>
2025-07-11 00:39:38 +0530

    Recreating the homepage

--- client/gamergram/src/components/SteamLoginButton.jsx
+++ client/gamergram/src/components/SteamLoginButton.jsx
@@ -1,12 +1,25 @@
 import steamIcon from "/steam-icon.png";
 
-const SteamLoginButton = () => (
-  <button className="flex items-center gap-3 bg-gradient-to-r from-[#171A21] via-[#2A2F36] to-[#1B2838] hover:brightness-110 text-white px-4 py-1 w-[240px] rounded-md shadow-md cursor-pointer transition-all duration-200">
-    <img src={steamIcon} alt="Steam" className="w-10 h-10 ml-3" />
-    <span className="text-center text-white orbitron font-semibold">
-     Sign-in with Steam
-    </span>
-  </button>
-);
+const SteamLoginButton = () => {
+  const steamLogin = async () => {
+    window.location.href =
+      "https://38e1-2405-201-400d-3862-ec7c-bb44-9119-d254.ngrok-free.app/steam/steamAuth";
+  };
+
+  return (
+    <>
+      {" "}
+      <button
+        className="flex items-center gap-3 bg-gradient-to-r from-[#171A21] via-[#2A2F36] to-[#1B2838] hover:brightness-110 text-white px-4 py-1 w-[240px] rounded-md shadow-md cursor-pointer transition-all duration-200"
+        onClick={steamLogin}
+      >
+        <img src={steamIcon} alt="Steam" className="w-10 h-10 ml-3" />
+        <span className="text-center text-white orbitron font-semibold">
+          Sign-in with Steam
+        </span>
+      </button>
+    </>
+  );
+};
 
 export default SteamLoginButton;
---
+++ client/gamergram/src/components/TrendingGames.jsx
@@ -0,0 +1,82 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import gameNames from "../utils/gameNames";
+
+const TrendingGames = () => {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+        const gameReq = gameNames.map(async (gameName) => {
+          const req = await fetch(
+            `https://api.rawg.io/api/games?key=${apiKey}&search=${encodeURIComponent(
+              gameName
+            )}`,
+            {
+              method: "GET",
+              headers: { "content-type": "application/json" },
+            }
+          );
+
+          const data = await req.json();
+          return data.results[0];
+        });
+
+        const gameData = await Promise.all(gameReq);
+        setGames(gameData);
+      } catch (error) {
+        console.error("Failed to fetch images ", error.message);
+      }
+    };
+
+    fetchGames();
+  }, []);
+
+  return (
+    <div>
+      <div className="px-4 pt-60 md:pt-80">
+        <motion.h1
+          className="text-[#00CFFF] font-bold text-xl md:text-2xl"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          TRENDING GAMES
+        </motion.h1>
+        <div className="mt-4 flex gap-x-6 overflow-x-auto flex-nowrap no-scrollbar">
+          {games.length > 0 && (
+            <motion.div
+              className="flex gap-x-6 flex-nowrap w-max "
+              initial={{ x: 0 }}
+              animate={{ x: ["0%", "-50%"] }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "loop",
+                duration: 150,
+                ease: "linear",
+              }}
+            >
+              {/* Duplicate the game cards for looping */}
+              {[...games, ...games].map((card, index) => (
+                <div key={index} className="min-w-[8rem]">
+                  <img
+                    src={card?.background_image}
+                    className="rounded-xl object-cover h-35 w-32 md:h-36 md:w-36 lg:h-44 lg:w-[200px]"
+                    alt={card?.name}
+                    loading="lazy"
+                  />
+                  <p className="text-white font-semibold orbitron text-sm md:text-base mt-2 text-center">
+                    {card?.name}
+                  </p>
+                </div>
+              ))}
+            </motion.div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingGames;
